513ed38472d84543fa1301627d487a2b50185aee
Translator <dictate_liege.6q@icloud.com>
2025-04-17 17:20:57 +0200

    Add converter and update the files

---
+++ convert.sh
@@ -0,0 +1,22 @@
+pandoc ru.md -o ru.pdf \
+  --pdf-engine=xelatex \
+  -f markdown+superscript \
+  -V mainfont="Georgia" \
+  -V fontsize=11pt \
+  -V lang=ru \
+  -V otherlang=uk \
+  -V geometry='a4paper,left=1.5cm,right=1.5cm,top=2cm,bottom=2cm,headheight=12pt,headsep=6pt,footskip=14pt' \
+  --include-in-header=header.tex \
+  --include-before-body=inscription.ru.tex
+
+pandoc ua.md -o ua.pdf \
+  --pdf-engine=xelatex \
+  -f markdown+superscript \
+  -V mainfont="Georgia" \
+  -V fontsize=11pt \
+  -V lang=ua \
+  -V otherlang=uk \
+  -V geometry='a4paper,left=1.5cm,right=1.5cm,top=2cm,bottom=2cm,headheight=12pt,headsep=6pt,footskip=14pt' \
+  --include-in-header=header.tex \
+  --include-before-body=inscription.ua.tex
+
